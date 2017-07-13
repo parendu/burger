e11912018c2d52c59ba5074f240b7a5c72e3b6cb
parendu <parendu_parikh@yahoo.com>
2017-07-13 15:32:59 -0400

    commiting final working app

--- config/connection.js
+++ config/connection.js
@@ -6,28 +6,27 @@ var mysql = require("mysql");
 //provide user/password and db info
 var connection;
 
-// console.log(" connecting: JSWDB" + process.env.JAWSDB_URL);
+console.log(" connecting: JSWDB" + process.env.JAWSDB_URL);
 
-// if(process.env.JAWSDB_URL) {
-// 		console.log(" connecting: JSWDB" );
+if(process.env.JAWSDB_URL) {
+		console.log(" connecting: JSWDB" );
 
-// 	connection=mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-// 		console.log(" connecting: LOCAL" );
-
-
+	connection=mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+		console.log(" connecting: LOCAL" );
 
 
-	
+
+
+}	
 connection = mysql.createConnection({
 	host: "localhost",
 	PORT: 3306,
 	user: "root",
-
 	password: "password",
 	database: "burger_db"
 });
-
+};
 //connection database
 connection.connect(function(err){
 	if (err) {
